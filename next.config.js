d6c2bfd3ab5cf807d8d9fc3942ab2cdce7496779
Reza Morsali <light.reza@gmail.com>
2021-06-26 08:58:28 +0430

    add mdx

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 const withPlugins = require('next-compose-plugins')
+const withMdxEnhanced = require("next-mdx-enhanced");
 
 const nextConfig = {
   reactStrictMode: true,
 }
-module.exports = withPlugins([
 
+module.exports = withPlugins([
+  withMdxEnhanced({
+    layoutPath: "components/layouts",
+    defaultLayout: true,
+    fileExtensions: ["mdx", "md"],
+    remarkPlugins: [],
+    rehypePlugins: [],
+  })
 ], nextConfig)
