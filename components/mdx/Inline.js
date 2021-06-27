74f90c7e7b2b5d84f3d7077934a75708789e1be2
Reza Morsali <light.reza@gmail.com>
2021-06-27 14:24:49 +0430

    change elements color to theme

--- components/mdx/Inline.js
+++ components/mdx/Inline.js
@@ -1,2 +1,2 @@
 export const Inline = props =>
-  <code className="bg-gray-100 border rounded px-1 py-0.5 text-sm border-gray-300 font-mono " {...props}/>
+  <code className="bg-indigo-100 border rounded px-1 py-0.5 text-sm border-indigo-300 text-indigo-600 font-mono " {...props} />
--- components/mdx/Text.js
+++ components/mdx/Text.js
@@ -1,6 +1,6 @@
 
 export const Paragraph = props =>
-  <p className="text-gray-700 font-normal my-5 leading-7" {...props} />
+  <p className="text-gray-700 font-normal my-4 leading-7" {...props} />
 
 export const Link = props =>
-  <a className="font-medium text-blue-500 hover:underline hover:text-blue-500 transition-all duration-100" {...props} />
+  <a className="font-medium text-indigo-500 hover:underline hover:text-indigo-600 transition-all duration-100" {...props} />
