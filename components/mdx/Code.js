524c2c42d293488fe5590414e93c0e639bcf33d5
Reza Morsali <light.reza@gmail.com>
2021-06-27 14:23:40 +0430

    fix: set default language to ""

--- components/mdx/Code.js
+++ components/mdx/Code.js
@@ -2,7 +2,7 @@ import Highlight, { defaultProps } from "prism-react-renderer";
 import theme from "prism-react-renderer/themes/palenight";
 
 export const Code = ({ children, className }) => {
-  const language = className.replace(/language-/, "");
+  const language = className?.replace(/language-/, "") || "";
   return (
     <Highlight {...defaultProps} theme={theme} code={children.trim()} language={language} >
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
