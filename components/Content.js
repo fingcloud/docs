bec2c5edc14154f1dca5f02415544feabb6b8339
Reza Morsali <light.reza@gmail.com>
2021-06-27 14:48:13 +0430

    change link to dashboard

--- components/Content.js
+++ components/Content.js
@@ -4,8 +4,8 @@ import { FiArrowLeft } from 'react-icons/fi'
 export const Content = ({ children }) => (
   <div className="w-full">
     <header className="flex items-center justify-end pt-6 pb-2 px-8">
-      <Link href="https://dashboard.fing.ir/login" passHref>
-        <a className="text-gray-600 hover:text-gray-700">ورود<FiArrowLeft className="inline-block mr-2" /></a>
+      <Link href="https://dashboard.fing.ir/" passHref>
+        <a className="bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-1.5 rounded transition-all duration-100">داشبورد<FiArrowLeft className="inline-block mr-2" /></a>
       </Link>
     </header>
     <main className="px-8">
