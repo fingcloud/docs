aad42c57b7dcde46d9ce6a3a9962fefe8862d06e
Reza Morsali <light.reza@gmail.com>
2021-06-27 14:08:01 +0430

    chore: change theme

--- components/mdx/Code.js
+++ components/mdx/Code.js
@@ -1,5 +1,5 @@
 import Highlight, { defaultProps } from "prism-react-renderer";
-import theme from "prism-react-renderer/themes/dracula";
+import theme from "prism-react-renderer/themes/palenight";
 
 export const Code = ({ children, className }) => {
   const language = className.replace(/language-/, "");
