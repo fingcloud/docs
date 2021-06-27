bf1c52e7daa1dc24666ee26859b3d3ca2b65da36
Reza Morsali <light.reza@gmail.com>
2021-06-27 14:24:19 +0430

    align paddings

--- components/Layout.js
+++ components/Layout.js
@@ -2,7 +2,6 @@ import { Sidebar } from './Sidebar'
 import { Content } from './Content'
 
 export default function Layout({ children, meta }) {
-  console.log(meta)
   return (
     <div className="min-h-screen relative flex max-w-6xl mx-auto">
       <Sidebar />
@@ -10,8 +9,6 @@ export default function Layout({ children, meta }) {
     </div>
   )
 }
-
-
 
 export const withLayout = (matter) => {
   const Layout = ({ children }) => <Layout>{children}</Layout>
--- components/mdx/Heading.js
+++ components/mdx/Heading.js
@@ -8,17 +8,17 @@ export const H2 = (props) => (
 )
 
 export const H3 = (props) => (
-  <h3 className="text-gray-800 text-xl font-semibold mb-4 mt-8" {...props} />
+  <h3 className="text-gray-800 text-xl font-semibold mb-2 mt-8" {...props} />
 )
 
 export const H4 = (props) => (
-  <h4 className="text-gray-800 text-lg font-semibold mb-3 mt-8" {...props} />
+  <h4 className="text-gray-800 text-lg font-semibold mb-2 mt-8" {...props} />
 )
 
 export const H5 = (props) => (
-  <h5 className="text-gray-800 font-semibold mb-2 mt-8" {...props} />
+  <h5 className="text-gray-800 font-semibold mb-1 mt-8" {...props} />
 )
 
 export const H6 = (props) => (
-  <h5 className="text-gray-800 text-sm font-semibold mb-1.5" {...props} />
+  <h5 className="text-gray-800 text-sm font-semibold mb-1" {...props} />
 )
