60f74203cc7417638e51486053d560d541380f1d
Reza Morsali <light.reza@gmail.com>
2021-06-27 08:46:19 +0430

    config docs

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 const withPlugins = require('next-compose-plugins')
 
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+})
+
 const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
   reactStrictMode: true,
 }
 
 module.exports = withPlugins([
+  withMDX
 ], nextConfig)
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
+import Layout from 'components/Layout'
+import { MDXProvider } from "@mdx-js/react"
 import 'styles/globals.css'
 import 'tailwindcss/tailwind.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  console.log('Component', Component)
+  console.log('pageProps', pageProps)
+  return (
+    <MDXProvider>
+      <Component {...pageProps} />
+    </MDXProvider>
+  )
 }
 
 export default MyApp
