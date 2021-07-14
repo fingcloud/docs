a0941147dc31672c805e052f65f41b86a35029b4
Abolfazl <abolfazl.gol.6565@gmail.com>
2021-07-14 11:56:52 +0430

    commit git & change overview slug

--- data/sidebar.js
+++ data/sidebar.js
@@ -2,7 +2,7 @@ export const sidebarItems = [
   {
     title: 'اطلاعات کلی',
     pages: [
-      { label: 'معرفی فینگ', slug: '/' },
+      { label: 'معرفی فینگ', slug: '/introduction' },
     ]
   },
   {
@@ -13,12 +13,12 @@ export const sidebarItems = [
       { label: 'دستورات', slug: '/cli/commands' },
     ]
   },
-  {
-    title: 'Git',
-    pages: [
-      { label: 'گیت ‌هاب', slug: '/git/fing-github' } 
-    ]
-  },
+  // {
+  //   title: 'Git',
+  //   pages: [
+  //     { label: 'گیت ‌هاب', slug: '/git/fing-github' } 
+  //   ]
+  // },
   {
     title: 'دامنه‌ها',
     pages: [
@@ -36,6 +36,12 @@ export const sidebarItems = [
     title: 'متغیرها ENV',
     pages: [
       { label: 'متغیرها', slug: '/env/environment-variables' },
+    ]
+  },
+  {
+    title: 'لاگ‌ها',
+    pages:[
+      {label:'لاگ‌ها', slug:'/logs'},
     ]
   }
 ]
