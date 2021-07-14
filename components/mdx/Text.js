d670e169dd237caeba447cd38ead90b205bde91c
Abolfazl <abolfazl.gol.6565@gmail.com>
2021-07-14 11:55:18 +0430

    add relative class

--- components/mdx/Text.js
+++ components/mdx/Text.js
@@ -3,4 +3,4 @@ export const Paragraph = props =>
   <p className="text-gray-700 font-normal my-4 leading-7" {...props} />
 
 export const Link = props =>
-  <a className="font-medium text-indigo-500 hover:underline hover:text-indigo-600 transition-all duration-100" {...props} />
+  <a className="font-medium text-indigo-500 hover:underline hover:text-indigo-600 transition-all duration-100 relative" {...props} />
