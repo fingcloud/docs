1e4893aa6fb1b5cb55915e7c889909d0fa8cdb1e
Abolfazl <abolfazl.gol.6565@gmail.com>
2021-07-14 11:54:54 +0430

    add group class

--- components/mdx/Heading.js
+++ components/mdx/Heading.js
@@ -1,24 +1,24 @@
 
 export const H1 = (props) => (
-  <h1 className="text-gray-800 text-3xl font-semibold mb-6 mt-8" {...props} />
+  <h1 className="text-gray-800 group text-3xl font-semibold mb-6 mt-8" {...props} />
 )
 
 export const H2 = (props) => (
-  <h2 className="text-gray-800 text-2xl font-semibold mb-4 mt-8" {...props} />
+  <h2 className="text-gray-800 group text-2xl font-semibold mb-4 mt-8" {...props} />
 )
 
 export const H3 = (props) => (
-  <h3 className="text-gray-800 text-xl font-semibold mb-2 mt-8" {...props} />
+  <h3 className="text-gray-800 group text-xl font-semibold mb-2 mt-8" {...props} />
 )
 
 export const H4 = (props) => (
-  <h4 className="text-gray-800 text-lg font-semibold mb-2 mt-8" {...props} />
+  <h4 className="text-gray-800 group text-lg font-semibold mb-2 mt-8" {...props} />
 )
 
 export const H5 = (props) => (
-  <h5 className="text-gray-800 font-semibold mb-1 mt-8" {...props} />
+  <h5 className="text-gray-800 group font-semibold mb-1 mt-8" {...props} />
 )
 
 export const H6 = (props) => (
-  <h5 className="text-gray-800 text-sm font-semibold mb-1" {...props} />
+  <h5 className="text-gray-800 group text-sm font-semibold mb-1" {...props} />
 )
