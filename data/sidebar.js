39e31ab48ec28ccbb6ddb5cf780b932ba8f009da
Abolfazl <abolfazl.gol.6565@gmail.com>
2021-07-18 13:21:38 +0430

    add platfroms file

--- data/sidebar.js
+++ data/sidebar.js
@@ -26,13 +26,6 @@ export const sidebarItems = [
     ]
   },
   {
-    title: 'پلتفرم',
-    pages: [
-      { label: 'برنامه‌ها', slug: '/platform/apps' },
-      { label: 'استقرارها', slug: '/platform/deployments' },
-    ]
-  },
-  {
     title: 'متغیرها ENV',
     pages: [
       { label: 'متغیرها', slug: '/env/environment-variables' },
@@ -40,8 +33,31 @@ export const sidebarItems = [
   },
   {
     title: 'لاگ‌ها',
-    pages:[
-      {label:'لاگ‌ها', slug:'/logs'},
+    pages: [
+      { label: 'لاگ‌ها', slug: '/logs' },
     ]
-  }
+  },
+  {
+    title: 'پلتفرم',
+    pages: [
+      { label: 'برنامه‌ها', slug: '/platform/apps' },
+      { label: 'استقرارها', slug: '/platform/deployments' },
+      { label: 'NodeJs', slug: '/platform/node' },
+      { label: 'Static', slug: '/platform/static' },
+      { label: 'Next', slug: '/platform/next' },
+      { label: 'Nuxt', slug: '/platform/nuxt' },
+      { label: 'React', slug: '/platform/react' },
+      { label: 'Laravel', slug: '/platform/laravel' },
+      { label: 'PHP', slug: '/platform/php' },
+      { label: 'Golang', slug: '/platform/golang' },
+      { label: 'Vue', slug: '/platform/vue' },
+      { label: 'Docker', slug: '/platform/docker' },
+      { label: 'Flask', slug: '/platform/flask' },
+      { label: 'Ruby', slug: '/platform/ruby' },
+      { label: 'Angular', slug: '/platform/angular' },
+      { label: 'Django', slug: '/platform/django' },
+      { label: 'React App', slug: '/platform/react-app' },
+      { label: 'Asp.Net', slug: '/platform/netcore' },
+    ]
+  },
 ]
