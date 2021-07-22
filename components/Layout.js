3b6aa886386ce0b2265d37967074fa9770006624
Reza Morsali <light.reza@gmail.com>
2021-07-22 12:02:31 +0430

    feat: add seo config to layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,37 @@
 import { Sidebar } from './Sidebar'
 import { Content } from './Content'
+import { useRouter } from 'next/dist/client/router'
+import { ArticleJsonLd, NextSeo } from 'next-seo'
 
 export default function Layout({ children, meta }) {
+  const router = useRouter()
+  const url = `https://docs.fing.ir${router.asPath}`
+
   return (
     <div className="min-h-screen relative flex max-w-6xl mx-auto">
+      <NextSeo
+        title={meta.title}
+        description={meta.description}
+        canonical={url}
+        openGraph={{
+          type: 'article',
+          title: meta.title,
+          description: meta.description,
+          url,
+        }}
+      />
+
+      < ArticleJsonLd
+        url={url}
+        description={meta.description}
+        title={meta.title}
+        images={[]}
+        datePublished={meta.created_at}
+        dateModified={meta.modified_at}
+        authorName="Fing"
+        publisherLogo="https://fing.ir/images/icon-dark.png"
+        publisherName="Fing"
+      />
       <Sidebar />
       <Content>{children}</Content>
     </div>
