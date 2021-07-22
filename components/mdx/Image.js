d984c1db028188514bd2ed260830dfaca3ed1264
Reza Morsali <light.reza@gmail.com>
2021-07-22 11:57:58 +0430

    fix: remove image parent div

--- components/mdx/Image.js
+++ components/mdx/Image.js
@@ -1,6 +1,4 @@
 
 export const Image = (props) => (
-  <div className="relative w-full">
-    <img className="my-4 rounded hover:shadow-lg border transition-all duration-200 mx-auto" {...props} />
-  </div>
+  <img className="my-4 w-full rounded hover:shadow-lg border transition-all duration-200 mx-auto" {...props} />
 )
