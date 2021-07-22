37c0fd9a225ce7a3df5a6c707643db4081c5da41
Reza Morsali <light.reza@gmail.com>
2021-07-22 11:57:47 +0430

    feat: add default seo config

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import { DefaultSeo } from "next-seo";
 import { MDXProvider } from "@mdx-js/react"
 import { components } from 'components/mdx'
 import NextNprogress from 'nextjs-progressbar';
@@ -8,6 +9,25 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <MDXProvider components={components}>
+      <DefaultSeo
+        title='مستندات فینگ'
+        description='مستندات سرویس ابری فینگ'
+        canonical='https://docs.fing.ir/'
+        titleTemplate='%s | مستندات فینگ'
+        openGraph={{
+          description: 'مستندات سرویس ابری فینگ',
+          type: 'website',
+          locale: 'fa_IR',
+          url: 'https://docs.fing.ir,',
+          site_name: 'مستندات فینگ',
+          title: 'مستندات فینگ'
+        }}
+        twitter={{
+          handle: '@fingcloud',
+          site: '@fingcloud',
+          cardType: 'summary_large_image',
+        }}
+      />
       <NextNprogress className="bg-indigo-500" color="#6366f1" options={{ showSpinner: false }} />
       <Component {...pageProps} />
     </MDXProvider>
