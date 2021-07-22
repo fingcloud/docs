d7cbee21d0dace14b4c0dd748d895b65860b7a6e
Reza Morsali <light.reza@gmail.com>
2021-07-22 11:58:16 +0430

    feat: add page seo

---
+++ components/PageSeo.js
@@ -0,0 +1,33 @@
+import { ArticleJsonLd, NextSeo } from "next-seo"
+
+export const PageSeo = ({ title, description, url, created_at, modified_at }) => {
+  const fullUrl = `https://docs.fing.ir${url}`
+
+  return (
+    <>
+      <NextSeo
+        title={title}
+        description={description}
+        canonical={fullUrl}
+        openGraph={{
+          type: 'article',
+          title: title,
+          description: description,
+          url,
+        }}
+      />
+
+      < ArticleJsonLd
+        url={fullUrl}
+        description={description}
+        title={title}
+        images={[]}
+        datePublished={created_at}
+        dateModified={modified_at}
+        authorName="Fing"
+        publisherLogo="https://fing.ir/images/icon-dark.png"
+        publisherName="Fing"
+      />
+    </>
+  )
+}
