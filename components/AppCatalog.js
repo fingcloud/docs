46cf2523f23a370554acb4bf89d252bddcc0e287
Reza Morsali <light.reza@gmail.com>
2021-07-25 13:33:21 +0430

    fix: rename icons path

--- components/AppCatalog.js
+++ components/AppCatalog.js
@@ -5,7 +5,7 @@ export const AppCatalog = ({ platform, title, href }) => (
   <Link href={href} passHref>
     <a href="#" className="ml-2 mb-2 text-gray-700 hover:text-gray-800 hover:shadow transition-all duration-150 ease-in-out">
       <div className="flex flex-col items-center space-y-2 cursor-pointer p-6 rounded-lg shadow transition duration-100 ease-in-out ">
-        <img src={`https://fing.ir/images/frameworks/${platform}.svg`} className="w-16 h-16" />
+        <img src={`https://fing.ir/images/platforms/${platform}.svg`} className="w-16 h-16" />
         <div>{title}</div>
       </div>
     </a>
--- components/PlatformHead.js
+++ components/PlatformHead.js
@@ -2,7 +2,7 @@
 
 export const PlatformHead = ({ platform, title }) => (
   <div className="flex center ">
-    <img src={`https://fing.ir/images/frameworks/${platform}.svg`} className="w-16 h-16" />
+    <img src={`https://fing.ir/images/platforms/${platform}.svg`} className="w-16 h-16" />
     <div className="mt-5 mr-3 text-2xl">{title}</div>
   </div>
 )
