11a3e3befc5726c6a4dac3cbc27afcd3efd1bbcc
ali <light.ali.71@gmail.com>
2021-07-26 14:11:59 +0430

    change line height

--- components/mdx/Text.js
+++ components/mdx/Text.js
@@ -1,6 +1,6 @@
 
 export const Paragraph = props =>
-  <p className="text-gray-700 font-normal my-4 leading-7" {...props} />
+  <p className="text-gray-700 font-normal my-4 leading-8" {...props} />
 
 export const Link = props =>
   <a className="font-medium text-blue-500 hover:underline hover:text-blue-600 transition-all duration-100 relative" {...props} />
