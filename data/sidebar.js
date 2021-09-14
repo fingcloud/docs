f7e6595fdc5912876c75ccbc10ac9e76b98183cd
Abolfazl <abolfazl.gol.6565@gmail.com>
2021-09-14 17:30:32 +0430

    add installation and connection content to postgreSQL

--- data/sidebar.js
+++ data/sidebar.js
@@ -42,6 +42,7 @@ export const sidebarItems = [
     title: 'دیتابیس',
     pages: [
       { label: 'MySQL', slug: '/database/mysql' },
+      { label: 'PostgreSQL', slug: '/database/postgresql' },
       { label: 'MongoDB', slug: '/database/mongodb' },
     ]
   },
