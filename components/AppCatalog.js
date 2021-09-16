4c1edd93d74745dd1bbfffcc8d551995f0199db7
Abolfazl <abolfazl.gol.6565@gmail.com>
2021-09-16 13:20:10 +0430

    fix: change src

--- components/AppCatalog.js
+++ components/AppCatalog.js
@@ -1,11 +1,11 @@
 
 import Link from 'next/link'
 
-export const AppCatalog = ({ platform, title, href, app }) => (
+export const AppCatalog = ({ platform, title, href, src }) => (
   <Link href={href} passHref>
     <a href="#" className="ml-2 mb-2 text-gray-700 hover:text-gray-800 hover:shadow transition-all duration-150 ease-in-out">
       <div className="flex flex-col items-center space-y-2 cursor-pointer p-6 rounded-lg shadow transition duration-100 ease-in-out ">
-        <img src={`${app ? `https://fing.ir/images/platforms/${platform}.svg` : `https://raw.githubusercontent.com/devicons/devicon/master/icons/${platform}/${platform}-original.svg`}`} className="w-16 h-16" />
+        <img src={`${platform ? `https://fing.ir/images/platforms/${platform}.svg` : `${src}`}`} className="w-16 h-16" />
         <div>{title}</div>
       </div>
     </a>
