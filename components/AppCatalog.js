44b4a995848c65027b55af8892645453075ebd45
Abolfazl <abolfazl.gol.6565@gmail.com>
2021-09-18 17:23:36 +0430

    commit

--- components/AppCatalog.js
+++ components/AppCatalog.js
@@ -1,11 +1,11 @@
 
 import Link from 'next/link'
 
-export const AppCatalog = ({ platform, title, href, app }) => (
+export const AppCatalog = ({ platform, title, href, src }) => (
   <Link href={href} passHref>
     <a href="#" className="mb-2 ml-2 text-gray-700 transition-all duration-150 ease-in-out hover:text-gray-800 hover:shadow">
       <div className="flex flex-col items-center p-6 space-y-2 transition duration-100 ease-in-out rounded-lg shadow cursor-pointer ">
-        <img src={`${app ? `https://fing.ir/images/platforms/${platform}.svg` : `https://raw.githubusercontent.com/devicons/devicon/master/icons/${platform}/${platform}-original.svg`}`} className="w-16 h-16" />
+        <img src={`${platform ? `https://fing.ir/images/platforms/${platform}.svg` : `${src}`}`} className="w-16 h-16" />
         <div>{title}</div>
       </div>
     </a>
