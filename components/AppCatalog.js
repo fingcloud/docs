f69021c37dcf2f66a477da73bd625c43121c2133
Abolfazl <abolfazl.gol.6565@gmail.com>
2021-10-21 13:26:14 +0330

    add alt tag to img (close #15)

--- components/AppCatalog.js
+++ components/AppCatalog.js
@@ -5,7 +5,7 @@ export const AppCatalog = ({ platform, title, href, src }) => (
   <Link href={href} passHref>
     <a href="#" className="mb-2 ml-2 text-gray-700 transition-all duration-150 ease-in-out hover:text-gray-800 hover:shadow">
       <div className="flex flex-col items-center p-6 space-y-2 transition duration-100 ease-in-out rounded-lg shadow cursor-pointer ">
-        <img src={`${platform ? `https://fing.ir/images/platforms/${platform}.svg` : `${src}`}`} className="w-16 h-16" />
+        <img src={`${platform ? `https://fing.ir/images/platforms/${platform}.svg` : `${src}`}`} alt={`${title}`} className="w-16 h-16" />
         <div>{title}</div>
       </div>
     </a>
