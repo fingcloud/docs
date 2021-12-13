bdd0f1c67a4eb62bde173f634468614d79c87c50
Abolfazl <abolfazl.gol.6565@gmail.com>
2021-12-13 16:59:47 +0330

    fix: change icons

--- components/PlatformHead.js
+++ components/PlatformHead.js
@@ -1,8 +1,8 @@
 
 
-export const PlatformHead = ({ platform, title, src }) => (
+export const PlatformHead = ({ title, icon, src }) => (
   <div className="flex center ">
-    <img src={`${platform ? `https://fing.ir/images/platforms/${platform}.svg` : `${src}`}`} alt={title} className="w-16 h-16" />
+    <img src={`${icon ? `https://raw.githubusercontent.com/fingcloud/devicon/master/icons/${icon}` : `${src}`}`} alt={title} className="w-16 h-16" />
     <div className="mt-5 mr-3 text-2xl">{title}</div>
   </div>
 )
