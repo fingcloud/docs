ca48dac29193eb372f0761e7e46d9a5f13dc36f2
Ali <mr.mah.2014@gmail.com>
2022-03-06 17:41:53 +0330

    make catalog grid

--- components/AppCatalog.js
+++ components/AppCatalog.js
@@ -3,9 +3,9 @@ import Link from 'next/link'
 
 export const AppCatalog = ({ title, href, icon, src }) => (
   <Link href={href} passHref>
-    <a href="#" className="mb-2 ml-2 text-gray-700 transition-all duration-150 ease-in-out hover:text-gray-800 hover:shadow">
-      <div className="flex flex-col items-center p-6 space-y-2 transition duration-100 ease-in-out rounded-lg shadow cursor-pointer ">
-        <img src={`${icon ? `https://fing.ir/images/platforms/${icon}.svg` : `${src}`}`} className="w-16 h-16" />
+    <a href="#" className="mb-2 ml-2 text-gray-700 transition-all duration-150 ease-in-out hover:text-gray-800">
+      <div className="flex flex-col items-center p-6 space-y-2 transition duration-200 ease-in-out rounded border border-gray-150 cursor-pointer hover:border-blue-600 hover:bg-blue-50">
+        <img src={`${icon ? `https://raw.githubusercontent.com/fingcloud/devicon/master/icons/${icon}` : `${src}`}`} className="w-16 h-16" />
         <div>{title}</div>
       </div>
     </a>
@@ -13,5 +13,5 @@ export const AppCatalog = ({ title, href, icon, src }) => (
 )
 
 export const Catalog = (props) => (
-  <div className="flex flex-wrap" {...props} />
+  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7" {...props} />
 )
