85905e8b36ac745e08da76c775598649790077b6
Reza <light.reza@gmail.com>
2022-03-14 12:47:46 +0330

    update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 const colors = require('tailwindcss/colors')
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './next.config.js'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './next.config.js'],
   theme: {
     extend: {
       colors: {
