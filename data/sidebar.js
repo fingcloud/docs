9827ede7638be9f5371f0b337fb07eabf19a684b
abolfazl-gol <abolfazl.gol.6565@gmail.com>
2022-05-24 13:42:54 +0430

    add vite page to quickstart

--- data/sidebar.js
+++ data/sidebar.js
@@ -24,6 +24,7 @@ export const sidebarItems = [
       { label: 'اجرای برنامه Gatsby', slug: '/quickstart/gatsby' },
       { label: 'اجرای برنامه Nuxt', slug: '/quickstart/nuxt' },
       { label: 'اجرای برنامه Nest', slug: '/quickstart/nest' },
+      { label: 'اجرای برنامه Vite', slug: '/quickstart/vite' },
     ]
   },
 
